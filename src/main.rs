4b285252b42e8dc48d20d71172c143dfecd99d5a
Adele Dewey-Lopez <adeletlopez@gmail.com>
2014-11-16 22:29:38 -0500

    implement subtraction

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,34 @@ mod poly {
          }
       }
    }
+
+   impl Sub<Polynomial, Polynomial> for Polynomial {
+      fn sub(&self, rhs: &Polynomial) -> Polynomial {
+         // shift is the degree_shift of the sum
+         let shift = cmp::min(self.degree_shift, rhs.degree_shift);
+         // degree is the degree of the sum
+         let degree = cmp::max::<int>(self.degree_shift + self.terms.len() as int, 
+                               rhs.degree_shift + rhs.terms.len() as int);
+         // so the difference degree - shift is the total size of the sum
+         let mut vec: Vec<int> = Vec::new();
+         vec.reserve((degree - shift) as uint);
+     
+         for t in range(0, degree - shift) {
+            let ai = t + shift - self.degree_shift;
+            let bi = t + shift - rhs.degree_shift;
+            let a = if ai < 0 || ai >= self.terms.len() as int { 0 } 
+                     else { self.terms[ai as uint] };
+            let b = if bi < 0 || bi >= rhs.terms.len() as int { 0 } 
+                     else { rhs.terms[bi as uint] };
+            vec.push(a-b);
+         }
+
+         Polynomial {
+            degree_shift: shift,
+            terms: vec,
+         }
+      }
+   }
 }
 
 #[test]
@@ -138,6 +166,33 @@ fn additive_inverse () {
    assert_eq!(p + q, zero);
 }
 
+#[test]
+fn subtract_from_self () {
+    let p = poly::Polynomial {
+      degree_shift: 0,
+      terms: vec![1i, 2i, 3i, 4i],
+   };
+  
+   let zero = poly::Polynomial {
+      degree_shift: 4,
+      terms: vec![0i],
+   };
+   assert_eq!(p - p, zero);
+}
+
+#[test]
+fn subtract_zero () {
+    let p = poly::Polynomial {
+      degree_shift: 3,
+      terms: vec![1i, 2i, 3i, 4i],
+   };
+   let zero = poly::Polynomial {
+      degree_shift: 4,
+      terms: vec![0i],
+   };
+   assert_eq!(p - zero, p);
+}
+
 #[cfg(not(test))]
 fn main() {
    let p = poly::Polynomial {
@@ -150,7 +205,7 @@ fn main() {
       terms: vec![2i, 4, 0, -6],
    };
 
-   println!("{}", p + q);
+   println!("{}", p - q);
 } 
 
 
